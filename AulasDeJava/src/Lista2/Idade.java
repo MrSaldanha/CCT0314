f5a614a98fc1b6dc0fc7ec3f356b3367bb72d4a9
MrSaldanha <sistema.eduardo@gmail.com>
2016-09-02 04:40:50 -0300

    Exercísios

---
+++ AulasDeJava/src/Lista2/Idade.java
@@ -0,0 +1,30 @@
+
+package Lista2;
+
+import javax.swing.JOptionPane;
+
+public class Idade {
+    
+    public static void main(String[] args) {
+        int idade,i ;
+        idade = Integer.parseInt(JOptionPane.showInputDialog("Informe sua idade: "));
+        
+        i = analisaIdade(idade);
+        
+    }
+    
+    public static int analisaIdade(int idade) {
+        if (idade >= 21)
+            JOptionPane.showMessageDialog(null,"Você já é maior de idade!");
+        else JOptionPane.showMessageDialog(null,"Você é menor de idade!");
+        
+        return idade;
+        
+        /*
+        Para utilizar o operador condicional:
+        JOptionPane.showMessageDialog(null,idade >= 21 ? "Você já é maior de idade!" : "Você é menor de idade!");
+        */
+              
+    }
+    
+}
